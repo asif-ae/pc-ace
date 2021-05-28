51930f5ace79ef01c83957f1e308e2546e11b97c
Asif Estiak <me.asif.ae@gmail.com>
2021-05-28 20:57:24 +0600

    initialized appJs branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <Fragment>
+      <Router>
+        <Switch>
+          <Route path="/">
+            
+          </Route>
+          <Route path="/">
+            
+          </Route>
+        </Switch>
+      </Router>
     </Fragment>
   );
 }
