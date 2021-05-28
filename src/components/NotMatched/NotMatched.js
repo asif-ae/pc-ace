b8f641d3006390a40214fdf936e9d7ae02ecb31c
Asif Estiak <me.asif.ae@gmail.com>
2021-05-28 21:32:02 +0600

    new font family added

--- src/components/NotMatched/NotMatched.js
+++ src/components/NotMatched/NotMatched.js
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react';
 const NotMatched = () => {
   return (
     <Fragment>
-      <div className="d-flex flex-column align-items-center justify-content-center" style={{height: "100vh"}}>
-        <h1>404</h1>
-        <h3 className="">Item Not Found</h3>
+      <div className="d-flex flex-column align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+        <h1 className="display-1">404</h1>
+        <h3 className="text-uppercase">Page Not Found!</h3>
       </div>
     </Fragment>
   );
