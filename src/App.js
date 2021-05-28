811781750a6b2d8f9338367500333db0f51c7980
Asif Estiak <me.asif.ae@gmail.com>
2021-05-28 20:00:45 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
