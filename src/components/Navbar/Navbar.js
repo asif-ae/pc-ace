84bc3508c42ade0f3af891ecb32acdf0e99f7055
Afroza Akter Ruma <afroza.akter.ruma035@gmail.com>
2021-05-29 17:05:59 +0600

    updated navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <Link to="/">
-        <h2 className="navbar-logo">Redux Shopping Cart</h2>
-      </Link>
-      <Link to="/">
-        <div className="navbar-cart">
-          <h3 className="cart-title">Cart</h3>
-          <img className="cart-image" src="" alt="shopping cart" />
-          <div className="cart-counter">1</div>
+    <Fragment>
+      <nav
+        className="navbar navbar-expand-lg navbar-style"
+        style={{ color: "navy" }}
+      >
+        <div className="container-fluid d-flex align-items-center">
+          <Link className="navbar-brand text-uppercase" to="/">
+            PC Ace
+          </Link>
+          <button
+            className="navbar-toggler menu-button-styles"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <FontAwesomeIcon icon={faEllipsisV} className="menu-icon-styles" />
+          </button>
+          <div
+            className="collapse navbar-collapse w-100"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav align-items-center justify-content-end w-100">
+              <div className="pe-3 d-none d-lg-block">
+                <Link className="nav-link text-uppercase" to="/">
+                  Home
+                </Link>
+              </div>
+              <div className="py-3 d-lg-none">
+                <Link className="nav-link text-uppercase" to="/">
+                  Home
+                </Link>
+              </div>
+              <Link className="nav-link text-uppercase" to="/cart">
+                <span className="bg-white py-2 px-3 rounded-pill cart-items-amount">
+                  <span className="px-1">
+                    <FontAwesomeIcon
+                      icon={faShoppingCart}
+                      className="cart-icon-styles"
+                    />
+                  </span>
+                  &nbsp;<span className="px-1">1</span>
+                </span>
+              </Link>
+            </div>
+          </div>
         </div>
-      </Link>
-      <Link className="nav-link active" to="/"></Link>
-      <button class="btn" type="login">
-        Login
-      </button>
-    </div>
+      </nav>
+    </Fragment>
   );
 };
 
