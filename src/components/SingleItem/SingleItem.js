c680446666dc4a726c85761b27594cc3c197a82c
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-05-30 11:20:12 +0600

    single Item update

--- src/components/SingleItem/SingleItem.js
+++ src/components/SingleItem/SingleItem.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const SingleItem = () => {
+const SingleItem = ({currentItem}) => {
     return (
-        <div>
-            
+        <div className={styles.singleItem}>
+            <img className="img-fluid" src={currentItem.image} alt="" />
+            <div>
+                <p>{currentItem.title}</p>
+                <p>{currentItem.description}</p>
+                <p>{currentItem.price}</p>
+                <button className="btn btn-primary">Add To Cart</button>
+            </div>
         </div>
     );
+};
+
+const mapStateToProps = (state) => {
+    return{
+        currentItem: state.shop.currentItem,
+    };
 };
 
 export default SingleItem;
