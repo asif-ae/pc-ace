2c07355940787604965e1cba4cbaa03c18c0f6ea
Asif Estiak <me.asif.ae@gmail.com>
2021-05-30 16:10:27 +0600

    added navbar in the notmatched component

--- src/components/NotMatched/NotMatched.js
+++ src/components/NotMatched/NotMatched.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const NotMatched = () => {
   return (
     <Fragment>
+      <Navbar></Navbar>
       <div className="d-flex flex-column align-items-center justify-content-center" style={{minHeight: "100vh"}}>
         <h1 className="display-1">404</h1>
         <h3 className="text-uppercase">Page Not Found!</h3>
